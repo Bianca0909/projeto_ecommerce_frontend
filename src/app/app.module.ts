ab72c0d8790c944b92e0c2e68864ae9822c0e39d
Bianca0909 <biancamedeiros0909@gmail.com>
2023-01-06 21:12:46 -0300

    Finalizado layout menu tela inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
